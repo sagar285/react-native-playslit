f473bb1e2b47c9d1445f3c5c6278398c47ab3831
sagar285 <anandguptasir@gmail.com>
2025-11-26 21:13:16 +0530

    commit

--- App.jsx
+++ App.jsx
@@ -1,84 +1,34 @@
-import { Alert, Image, TextInput, StyleSheet, Text, View, Button, Pressable } from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, Text, View,ScrollView, Image,Dimensions,RefreshControl } from 'react-native'
+import React, { useState } from 'react'
 
+const img1 = "https://images.unsplash.com/photo-1761839257469-96c78a7c2dd3?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
+const img2 = "https://plus.unsplash.com/premium_photo-1762487283482-840943bd5180?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+const img3 = "https://images.unsplash.com/photo-1763669029223-74f911a9e08b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8"
+const arrayofimages =[img1,img2,img3,img1,img2,img3,img1]
+const {width} =Dimensions.get("window")
 const App = () => {
-  const imgurl =
-    'http://images.unsplash.com/photo-1760243790660-a7958d7843ee?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D';
-  const [inputText,setinputtext] =useState()
-
-
-  const onPressFunction = () =>{
-    console.log("pressable function")
-    Alert.alert("pressable function")
-  }
-
-  
-  const onPressButton = () =>{
- Alert.alert("button function")
-  }
-  const onLoginPressButton = () =>{
- Alert.alert("onLoginPressButton function")
-  }
+  const [handlerefreshing,setrefreshing] =useState(false)
+  console.log(handlerefreshing,"handlerefreshing")
   return (
-    <View style={styles.View}>
-      {/* <Text style={[styles.text,{color:"red",fontSize:20}]}>App</Text> */}
-      {/* <Image
-      source={require("./img.png")} // local image
-      style={{width:300,height:300,objectFit:"contain"}} 
-      />
-      <Image // img from url
-      source={{uri:imgurl}} // local image
-      style={{wid<th:300,height:300,objectFit:"contain"}} 
-      /> */}
+    <ScrollView
+    showsHorizontalScrollIndicator={false}
+    // contentContainerStyle={{ width:300,padding:30}}
+    refreshControl={
+      <RefreshControl refreshing={handlerefreshing} onRefresh={()=>setrefreshing(true)}/>
+    }
+    >
+      {
+      arrayofimages.map((item)=>(
+        <Image
+        source={{uri:item}}
+        style={{width:width,height:200}}
+        />
+      ))
+      }
+    </ScrollView>
+  )
+}
 
-      <View style={styles.Inputview}>
-        {/* <TextInput 
-        style={styles.textInput}
-        placeholder='Enter any placeholder'
-        // readOnly={true}
-        placeholderTextColor={"red"} 
-        value={inputText}
-        onChangeText={(text) => setinputtext(text)}
-       keyboardType='number-pad'
-        /> */}
+export default App
 
-        <View style={{marginTop:12}}/>
-      </View>
-
-      <Pressable onPressOut={onPressButton} onLongPress={onLoginPressButton}>
-  <Text>I'm pressable!</Text>
-</Pressable>
-      {/* <Button 
-      onPress={onPressButton}
-        title='Button sUBMIT'
-        /> */}
-    </View>
-  );
-};
-
-export default App;
-
-const styles = StyleSheet.create({
-  View: {
-    display: 'flex',
-    // flex:1,
-    // justifyContent:"center",
-    alignItems: 'center',
-    // flexDirection: 'row',
-    paddingHorizontal: 14,
-    paddingVertical: 12,
-  },
-  Inputview: {
-    backgroundColor: 'white',
-    borderColor:"black",
-    borderWidth:1,
-  },
-  textInput:{
-   width:300,
-   height:100,
-   color:"blue"
-  },
-  text: {
-    // textAlign:"center"
-  },
-});
+const styles = StyleSheet.create({})
